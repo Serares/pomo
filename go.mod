5f473848bb207fb0fd3e443ff19587373b2176be
Rares <rjianu@abcam.com>
2023-08-20 15:16:33 +0300

    feat: Add interval tests and widgets

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/Serares/pomo
 
 go 1.20
+
+require github.com/mum4k/termdash v0.13.0
+
+require github.com/mattn/go-runewidth v0.0.9 // indirect
---
+++ pomodoro/inmemory_test.go
@@ -0,0 +1,14 @@
+package pomodoro_test
+
+import (
+	"testing"
+
+	"github.com/Serares/pomo/pomodoro"
+	"github.com/Serares/pomo/pomodoro/repository"
+)
+
+func getRepo(t *testing.T) (pomodoro.Repository, func()) {
+	t.Helper()
+	// return an empty cleanup function, because in-memory doesn't requre a cleanup functio
+	return repository.NewInMemoryRepo(), func() {}
+}
