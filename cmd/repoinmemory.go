5e474d6e768dee994a094bd7e32db5ee717a9618
Rares <rjianu@abcam.com>
2023-08-20 23:19:28 +0300

    feat: Use cobra cli to initialize the app

---
+++ cmd/repoinmemory.go
@@ -0,0 +1,10 @@
+package cmd
+
+import (
+	"github.com/Serares/pomo/pomodoro"
+	"github.com/Serares/pomo/pomodoro/repository"
+)
+
+func getRepo() (pomodoro.Repository, error) {
+	return repository.NewInMemoryRepo(), nil
+}
